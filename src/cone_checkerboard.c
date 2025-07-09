82a767fdc7533147e572ee6c8ab2820f6bf83bc4
KHR416 <hyy741@naver.com>
2025-07-09 17:18:11 +0900

    corrected uv mapping

--- src/cone_checkerboard.c
+++ src/cone_checkerboard.c
@@ -6,7 +6,7 @@
 /*   By: wchoe <wchoe@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 18:00:00 by chakim            #+#    #+#             */
-/*   Updated: 2025/07/08 16:36:38 by wchoe            ###   ########.fr       */
+/*   Updated: 2025/07/09 17:17:17 by wchoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,13 @@ t_vec3	cone_get_color(const t_intersect *record)
 		return (record->obj->color);
 	if (fabsf(record->p_local.v[2] - 1.0f) < EPSILON)
 	{
-		u = atan2f(record->p_local.v[1], record->p_local.v[0]) / (float)M_PI;
+		u = atan2f(record->p_local.v[1], record->p_local.v[0]) / (2.0f * (float)M_PI) + 0.5f;
 		v = sqrtf(record->p_local.v[0] * record->p_local.v[0] + \
 			record->p_local.v[1] * record->p_local.v[1]);
 	}
 	else
 	{
-		u = atan2f(record->p_local.v[1], \
-			record->p_local.v[0]) / (float)M_PI;
+		u = atan2f(record->p_local.v[1], record->p_local.v[0]) / (2.0f * (float)M_PI) + 0.5f;
 		v = record->p_local.v[2];
 	}
 	return (checkerboard_color_at(u, v));
--- src/cylinder_checkerboard.c
+++ src/cylinder_checkerboard.c
@@ -6,7 +6,7 @@
 /*   By: wchoe <wchoe@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 18:05:15 by chakim            #+#    #+#             */
-/*   Updated: 2025/07/08 16:36:38 by wchoe            ###   ########.fr       */
+/*   Updated: 2025/07/09 17:13:16 by wchoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ t_vec3	cylinder_get_color(const t_intersect *record)
 	if (fabsf(record->p_local.v[2] - 0.5f) < EPSILON || \
 	fabsf(record->p_local.v[2] + 0.5f) < EPSILON)
 	{
-		u = atan2f(record->p_local.v[1], record->p_local.v[0]) / (float)M_PI;
-		v = sqrtf(record->p_local.v[0] * record->p_local.v[0] + \
-			record->p_local.v[1] * record->p_local.v[1]);
+		u = atan2f(record->p_local.v[1], record->p_local.v[0])/(2.0f * M_PI) + 0.5f;
+		v = sqrtf(record->p_local.v[0] * record->p_local.v[0] + record->p_local.v[1] * record->p_local.v[1]);
+
 	}
 	else
 	{
-		u = asinf(record->p_local.v[1]) / (float)M_PI;
+		u = atan2f(record->p_local.v[1], record->p_local.v[0]) / (2.0f * (float)M_PI) + 0.5f;
 		v = 0.5f + record->p_local.v[2];
 	}
 	return (checkerboard_color_at(u, v));
