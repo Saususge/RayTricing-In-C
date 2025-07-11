d0b9cd3bc54aa4e19b6ab5958f5e2bedacaba160
KHR416 <hyy741@naver.com>
2025-07-11 16:04:57 +0900

    removed specular light from mandatory

--- src/hit_def_1.c
+++ src/hit_def_1.c
@@ -13,29 +13,6 @@
 #include "hit.h"
 #include "object.h"
 
-t_vec3	calculate_specular_light(const t_intersect *record, t_vec4 light_dir, \
-	float attenuation, t_vec3 light_intensity)
-{
-	t_vec4	p_world;
-	t_vec4	view_world;
-	t_vec4	reflect_world;
-	float	specular_dot;
-	float	specular_factor;
-
-	p_world = mat_mul_vec4(&record->obj->m, record->p_local);
-	view_world = vec4_sub(vec3_to_vec4(g()->cam.pos, 1.0f), p_world);
-	reflect_world = vec4_sub(vec4_neg(light_dir), \
-		vec4_mul(record->n_world, 2.0f * \
-			vec4_dot(vec4_neg(light_dir), record->n_world)));
-	reflect_world.v[3] = 0.0f;
-	specular_dot = fmaxf(vec4_dot(view_world, reflect_world) / \
-		(sqrtf(vec4_dot(view_world, view_world) * \
-		vec4_dot(reflect_world, reflect_world))), 0.0f);
-	specular_factor = powf(specular_dot, SHININESS) * \
-	K_SPECULAR * attenuation;
-	return (vec3_mul(light_intensity, specular_factor * 255.0f));
-}
-
 int	is_in_shadow(const t_intersect *record, int light_idx)
 {
 	t_vec4	p_world;
@@ -79,8 +56,6 @@ t_vec3	process_single_light(const t_intersect *record, int light_idx)
 	attenuation = calculate_attenuation(record, light_idx);
 	result = calculate_diffuse_light \
 	(record, light_dir, attenuation, g()->lights[light_idx].intensity);
-	result = vec3_add(result, calculate_specular_light \
-		(record, light_dir, attenuation, g()->lights[light_idx].intensity));
 	return (result);
 }
 
